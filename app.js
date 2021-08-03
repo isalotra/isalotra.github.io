83391134ffe0dde305ffebbaeb4666b78ae69d2e
isalotra <69400143+isalotra@users.noreply.github.com>
2021-08-03 23:30:28 +0200

    Add files via upload

---
+++ app.js
@@ -0,0 +1,26 @@
+const titreSpans = document.querySelectorAll('h1 span');
+const btns = document.querySelectorAll('.btn-first');
+const logo = document.querySelector('.logo');
+const medias = document.querySelectorAll('.bulle');
+const l1 = document.querySelector('.l1');
+const l2 = document.querySelector('.l2');
+const entete = document.querySelectorAll('.entete');
+
+window.addEventListener('load', () => {
+
+    const TL = gsap.timeline({paused: true});
+
+    TL
+    .staggerFrom(titreSpans, 1, {top: -50, opacity: 0, ease: "power2.out"}, 0.3)
+    .staggerFrom(btns, 1, {opacity: 0, ease: "power2.out"}, 0.3, '-=1')
+    .from(l1, 1, {width: 0, ease: "power2.out"}, '-=2')
+    .from(l2, 1, {width: 0, ease: "power2.out"}, '-=2')
+    .from(logo, 0.4, {transform: "scale(0)", ease: "power2.out"}, '-=2')
+    .staggerFrom(medias, 1, {right: -200, ease: "power2.out"}, 0.3, '-=1')
+    .staggerFrom(entete, 1, {opacity: 0, ease: "power2.out"}, 0.3, '-=2.5');
+
+    TL.play();
+})
+window.addEventListener("scroll", () => {
+    
+})
